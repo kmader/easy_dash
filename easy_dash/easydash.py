170e1689d04bdf366a9c2065141dd8cf97c84faf
Kevin Mader <kmader@users.noreply.github.com>
2019-04-07 18:55:08 +0200

    Adding full support for MPL plots (#2)
    * major updates with mpl additions
    
    * adding mpl
    
    * Update test_integration.py
    
    Tweaking setup
    
    * cleaning up requirements, adding pillow, updating tests to wait for graph
    
    * updating plotly export
    
    * changing conflicting names
    
    * combining tests together
    
    * tweaking names

--- easy_dash/easydash.py
+++ easy_dash/easydash.py
@@ -1,9 +1,82 @@
 """The easydash wrapper class"""
 from __future__ import print_function
+
 import inspect
 import os
+from functools import wraps
+
+import dash_html_components as html
+import dash_core_components as dcc
+import plotly.tools as tls
+from dash.dash import Dash
 from dash.dependencies import Input, Output
-from dash.dash import Dash  # noqa: F401
+
+from .viz import fig_to_uri
+
+
+def guess_io_args(func):
+    """Guesses the callback arguments from the signature.
+    >>> def update_src_of_output_5(arg_of_input): pass
+    >>> guess_io_args(update_src_of_output_5)
+    (<Output `output_5.src`>, [<Input `input.arg`>])
+    >>> def update_output_99(inp_5): pass
+    >>> guess_io_args(update_output_99)
+    (<Output `output_99.children`>, [<Input `inp_5.value`>])
+    """
+    valid_prefix_names = ["update_", "callback_"]
+
+    def check_prefix(in_name):
+        for prefix_name in valid_prefix_names:
+            if prefix_name in in_name:
+                return in_name.find(prefix_name) + len(prefix_name)
+
+        raise ValueError(
+            "auto_callback requires name to start with {}".format(valid_prefix_names)
+        )
+
+    def process_output(callback_func):
+        callback_name = getattr(callback_func, "__name__", "")
+        comp_prop_start_idx = check_prefix(callback_name)
+        comp_prop_name = callback_name[comp_prop_start_idx:]
+        property_start = comp_prop_name.rfind("_of_")
+        if property_start >= 1:
+            comp_name = comp_prop_name[property_start + 4 :]
+            prop_name = comp_prop_name[:property_start]
+        else:
+            # assume children if no underscore
+            comp_name = comp_prop_name
+            prop_name = "children"
+        return Output(component_id=comp_name, component_property=prop_name)
+
+    def process_input(callback_func):
+        if hasattr(inspect, "getfullargspec"):
+            spec_func = getattr(inspect, "getfullargspec")
+        elif hasattr(inspect, "getargspec"):
+            # pylint: disable=maybe-no-member, deprecated-method
+            spec_func = getattr(inspect, "getargspec")
+        else:
+            raise ValueError("Requires Python 2/3")
+
+        spec_args = spec_func(callback_func).args
+        input_list = []
+        for c_comp_prop_arg in spec_args:
+            property_start = c_comp_prop_arg.rfind("_of_")
+            if property_start >= 1:
+                prop_name = c_comp_prop_arg[:property_start]
+                comp_name = c_comp_prop_arg[property_start + 4 :]
+            else:
+                # assume value if no underscore
+                comp_name = c_comp_prop_arg
+                prop_name = "value"
+            input_list += [Input(component_id=comp_name, component_property=prop_name)]
+        return input_list
+
+    # process outputs
+    output = process_output(func)
+
+    # process inputs
+    inputs = process_input(func)
+    return output, inputs
 
 
 class EasyDash(Dash):
@@ -36,70 +109,42 @@ class EasyDash(Dash):
 
         """
 
-        valid_prefix_names = ["update_", "callback_"]
-
-        def check_prefix(in_name):
-            for prefix_name in valid_prefix_names:
-                if prefix_name in in_name:
-                    return in_name.find(prefix_name) + len(prefix_name)
-
-            raise ValueError(
-                "auto_callback requires name to start with {}".format(
-                    valid_prefix_names
-                )
-            )
-
-        def process_output(callback_func):
-            callback_name = getattr(callback_func, "__name__", "")
-            comp_prop_start_idx = check_prefix(callback_name)
-            comp_prop_name = callback_name[comp_prop_start_idx:]
-            property_start = comp_prop_name.rfind("_of_")
-            if property_start >= 1:
-                comp_name = comp_prop_name[property_start + 4 :]
-                prop_name = comp_prop_name[:property_start]
-            else:
-                # assume children if no underscore
-                comp_name = comp_prop_name
-                prop_name = "children"
-            return Output(component_id=comp_name, component_property=prop_name)
-
-        def process_input(callback_func):
-            if hasattr(inspect, "getfullargspec"):
-                spec_func = getattr(inspect, "getfullargspec")
-            elif hasattr(inspect, "getargspec"):
-                # pylint: disable=maybe-no-member, deprecated-method
-                spec_func = getattr(inspect, "getargspec")
-            else:
-                raise ValueError("Requires Python 2/3")
-
-            spec_args = spec_func(callback_func).args
-            input_list = []
-            for c_comp_prop_arg in spec_args:
-                property_start = c_comp_prop_arg.rfind("_of_")
-                if property_start >= 1:
-                    prop_name = c_comp_prop_arg[:property_start]
-                    comp_name = c_comp_prop_arg[property_start + 4 :]
-                else:
-                    # assume value if no underscore
-                    comp_name = c_comp_prop_arg
-                    prop_name = "value"
-                input_list += [
-                    Input(component_id=comp_name, component_property=prop_name)
-                ]
-            return input_list
-
         def wrap_callback(callback_func):
-            # process outputs
-            output = process_output(callback_func)
+            output, inputs = guess_io_args(callback_func)
             if debug:
                 print("Output:", output)
-            # process inputs
-            inputs = process_input(callback_func)
             if debug:
                 print("Inputs:", inputs)
             return self.callback(output, inputs=inputs)(callback_func)
 
         return wrap_callback
+
+    def mpl_callback(self, auto=True, use_plotly=False, dpi=None, **sv_args):
+        """Turns a matplotlib figure into a Dash object.
+        :param auto: automatically make callback
+        :param use_plotly: convert mpl to plotly
+        :param dpi:
+        :param sv_args:
+        :return:
+        """
+
+        def wrap_func(func):
+            @wraps(func)
+            def add_context(*args, **kwargs):
+                self.auto_callback(func)
+                out_fig = func(*args, **kwargs)
+                if use_plotly:
+                    return dcc.Graph(figure=tls.mpl_to_plotly(out_fig))
+                else:
+                    return html.Img(src=fig_to_uri(out_fig, dpi=dpi, **sv_args))
+
+            if auto:
+                output, inputs = guess_io_args(func)
+                return self.callback(output, inputs=inputs)(add_context)
+            else:
+                return add_context
+
+        return wrap_func
 
     def _repr_html_(self):
         return self.show_app()
@@ -133,5 +178,8 @@ class EasyDash(Dash):
             self.css.config.serve_locally = True
             self.scripts.config.serve_locally = True
         return self.run_server(
-            debug=False, host="0.0.0.0", port=port  # needs to be false in Jupyter
+            debug=False,
+            host="0.0.0.0",
+            port=port
+            # needs to be false in Jupyter
         )
--- tests/test_integration.py
+++ tests/test_integration.py
@@ -2,12 +2,16 @@ from multiprocessing import Value
 
 import dash_core_components as dcc
 import dash_html_components as html
+import matplotlib
+import matplotlib.pyplot as plt
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.common.keys import Keys
 
 from easy_dash import EasyDash
 from .IntegrationTests import IntegrationTests
 from .utils import assert_clean_console, invincible, wait_for
+
+matplotlib.use("Agg")
 
 
 class Tests(IntegrationTests):
@@ -21,12 +25,13 @@ class Tests(IntegrationTests):
 
         self.wait_for_element_by_id = wait_for_element_by_id
 
-    def test_auto_callback_default(self):
-        app = EasyDash("auto_callback_default")
+    def test_auto_callback(self):
+        app = EasyDash("auto_callback")
         app.layout = html.Div(
             [
                 dcc.Input(id="input", value="initial value"),
                 html.Div(html.Div([1.5, None, "string", html.Div(id="output1")])),
+                html.Div(id="output2"),
             ]
         )
 
@@ -37,10 +42,17 @@ class Tests(IntegrationTests):
             call_count.value = call_count.value + 1
             return input
 
+        @app.auto_callback()
+        def update_children_of_output2(value_of_input):
+            return value_of_input
+
         self.startServer(app, 8050)
 
         self.wait_for_text_to_equal("#output1", "initial value")
         self.percy_snapshot(name="auto-callback-1")
+
+        self.wait_for_text_to_equal("#output2", "initial value")
+        self.percy_snapshot(name="auto-callback-2")
 
         input1 = self.wait_for_element_by_id("input")
 
@@ -58,7 +70,10 @@ class Tests(IntegrationTests):
         input1.send_keys("hello world")
 
         self.wait_for_text_to_equal("#output1", "hello world")
-        self.percy_snapshot(name="auto-callback-2")
+        self.percy_snapshot(name="auto-callback-3")
+
+        self.wait_for_text_to_equal("#output2", "hello world")
+        self.percy_snapshot(name="auto-callback-4")
 
         self.assertEqual(
             call_count.value,
@@ -71,32 +86,38 @@ class Tests(IntegrationTests):
 
         assert_clean_console(self)
 
-    def test_auto_callback(self):
-        app = EasyDash("auto_callback")
+    def test_mpl_callback(self):
+        app = EasyDash("mpl_callback")
         app.layout = html.Div(
             [
-                dcc.Input(id="input", value="initial value"),
-                html.Div(html.Div([1.5, None, "string", html.Div(id="output1")])),
+                dcc.Input(id="plot_size", value="5"),
+                html.Div(id="output_mpl"),
+                html.Div(id="plot"),
             ]
         )
 
-        call_count = Value("i", 0)
-
         @app.auto_callback()
-        def update_children_of_output1(value_of_input):
-            call_count.value = call_count.value + 1
-            return value_of_input
+        def update_output_mpl(value_of_plot_size):
+            return "New Size: {}".format(value_of_plot_size)
 
-        self.startServer(app, 8051)
+        @app.mpl_callback()
+        def update_children_of_plot(value_of_plot_size):
+            height = float(value_of_plot_size)
+            fig, ax1 = plt.subplots(1, 1, figsize=(height, 5))
+            ax1.plot([0, 1, 1, 0], [0, 0, 1, 1], "r-.")
+            ax1.set_title(height)
+            return fig
 
-        self.wait_for_text_to_equal("#output1", "initial value")
-        self.percy_snapshot(name="auto-callback-3")
+        self.startServer(app, 8052)
 
-        input1 = self.wait_for_element_by_id("input")
+        self.wait_for_text_to_equal("#output_mpl", "New Size: 5")
+        self.percy_snapshot(name="mpl-callback-1")
+
+        input_ps = self.wait_for_element_by_id("plot_size")
 
         chain = (
             ActionChains(self.driver)
-            .click(input1)
+            .click(input_ps)
             .send_keys(Keys.HOME)
             .key_down(Keys.SHIFT)
             .send_keys(Keys.END)
@@ -105,18 +126,8 @@ class Tests(IntegrationTests):
         )
         chain.perform()
 
-        input1.send_keys("bye world")
-
-        self.wait_for_text_to_equal("#output1", "bye world")
-        self.percy_snapshot(name="auto-callback-4")
-
-        self.assertEqual(
-            call_count.value,
-            # an initial call to retrieve the first value
-            # and one for clearing the input
-            2 +
-            # one for each hello world character
-            len("bye world"),
-        )
+        input_ps.send_keys("20")
+        self.wait_for_text_to_equal("#output_mpl", "New Size: 20")
+        self.percy_snapshot(name="mpl-callback-2")
 
         assert_clean_console(self)
